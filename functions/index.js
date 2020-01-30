01db4f37129686aa53b44145a100c36f101506f3
Kenji Terai <kterai@e-vision.co.jp>
2020-01-30 18:29:28 +0900

    [functions] translate処理を追加

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,40 @@
 const functions = require('firebase-functions');
+const credentials = require('./credentials.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const { TranslationServiceClient } = require('@google-cloud/translate').v3beta1;
+const translationClient = new TranslationServiceClient({ credentials });
+
+
+const translateText = async text => {
+
+  const [response] = await translationClient.translateText({
+    parent: translationClient.locationPath(credentials.project_id, 'global'),
+    contents: [text],
+    mimeType: 'text/plain',
+    sourceLanguageCode: 'ja-JP',
+    targetLanguageCode: 'en-US',
+  });
+
+  return response.translations.map(t => t.translatedText).join('');
+}
+
+
+const express = require('express');
+const app = express();
+
+app.use(require('cors')());
+
+app.get('/', async (req, res) => {
+  try {
+    if (req.query.text) {
+      res.json({ translated: await translateText(req.query.text) });
+    } else {
+      res.status(400).send();
+    }
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+exports.translate = functions.https.onRequest(app);
